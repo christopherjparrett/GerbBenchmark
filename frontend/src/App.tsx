f0412607f19926e6806d3cb0921735ffe149e206
christopherjparrett <146243093+christopherjparrett@users.noreply.github.com>
2025-04-12 18:04:38 -0400

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,11 @@ import './App.css';
 
 import LoginPage from './pages/LoginPage';
 import CardPage from './pages/CardPage';
+import Home from './pages/Home';
+import Color from './pages/Color'
+import SignUp from './pages/SignUp'
+import Typing from './pages/Typing'
+import Reaction from './pages/Reaction'
 
 function App() {
   return (
@@ -10,6 +15,11 @@ function App() {
       <Routes>
         <Route path="/" element={<LoginPage />} />
         <Route path="/cards" element={<CardPage />} />
+        <Route path="/Home" element={<Home />} />
+        <Route path="/Color" element={<Color />} />
+        <Route path="/SignUp" element={<SignUp />} />
+        <Route path="/Typing" element={<Typing />} />
+        <Route path="/Reaction" element={<Reaction  />} />
       </Routes>
     </Router>
   );
