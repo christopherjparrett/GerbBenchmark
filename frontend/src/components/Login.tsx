71c700d79e52b829480a326c5720e5fe9c1a20c0
christopherjparrett <146243093+christopherjparrett@users.noreply.github.com>
2025-04-12 18:21:10 -0400

    Login redirects to homepage

--- frontend/src/components/Login.tsx
+++ frontend/src/components/Login.tsx
@@ -40,7 +40,7 @@ function Login()
                 localStorage.setItem('user_data', JSON.stringify(user));
   
                 setMessage('');
-                window.location.href = '/cards';
+                window.location.href = '/Home';
             }
         }
         catch(error:any)
