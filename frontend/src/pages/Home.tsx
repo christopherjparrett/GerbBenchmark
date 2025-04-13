843920cdec2c0d6e93dbf1e78e4aea54687fd12d
Javier <167123463+Nukkem13@users.noreply.github.com>
2025-04-13 18:40:33 -0400

    Update Home.tsx

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -15,9 +15,9 @@ const Home: React.FC = () => {
 
   return (
     <div className="Overlord" style={{backgroundImage: `url(${GerbPNG})`}}>
-        <profile />
         <div className='Banner'>
             <h1>Welcome to the Home Page!</h1>
+            <profile />
         </div>
       <button className = "HomeScreenButton" onClick={() => goTo('Color')}>
         <img src={ColorPNG} alt="Colors Button Logo" className="ButtonImage"></img>
