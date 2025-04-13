ebc38aec6c8362a4971815abb03ba37ec776b39d
Javier <167123463+Nukkem13@users.noreply.github.com>
2025-04-13 19:04:13 -0400

    Update Profile.tsx

--- frontend/src/components/Profile.tsx
+++ frontend/src/components/Profile.tsx
@@ -43,8 +43,8 @@ function Profile(){
           let subMenu = document.getElementById("subMenu");
 
           function toggleMenu(){
-            subMenu.classList.toggle("open-menu");
-          };
+            subMenu.classList.toggle("open-menu")
+          }
         </script>
       </>
   );
