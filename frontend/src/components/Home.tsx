542a0d3b6384c9526f8077a25aeb07a181046032
Javier <167123463+Nukkem13@users.noreply.github.com>
2025-04-13 18:24:27 -0400

    Update Home.tsx

--- frontend/src/components/Home.tsx
+++ frontend/src/components/Home.tsx
@@ -13,19 +13,38 @@ function profile(){
                     <li><a href="#">About</a></li>
                     <li><a href="#">Contact</a></li>
                   </ul>
-                  <img src="../assets/user.png" class="user-pic">  
+                  <img src="../assets/user.png" class="user-pic" onclick="toggleMenu()">  
 
-                  <div class="sub-menu-wrap">
+                  <div class="sub-menu-wrap" id="subMenu">
                     <div class="sub-menu">
                       <div class="user-info">
                         <img src="../assets/user.png"> 
-                        <h2>Matthew Gerber</>  
+                        <h3>Matthew Gerber</h3>  
                       </div>
+                        <hr>
+
+                        <a href="#" class="sub-menu-link">
+                          <img src="../assets/logo.png">
+                          <p>Log Out</p>
+                            <span>></span>
+
+                        <a href="#" class="sub-menu-link">
+                          <img src="../assets/logo.png">
+                          <p>Delete User</p>
+                            <span>></span>
                     </div>
                   </div>
                     
               </nav>
           </div>
+
+        <script>
+          let subMenu = document.getElementById("subMenu");
+
+          function toggleMenu(){
+            subMenu.classList.toggle()"open-menu");
+          }
+        </script>
   );
 };
 
