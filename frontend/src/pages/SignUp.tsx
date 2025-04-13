7d80c34b851a1ad771a05e142b5a134902dd3274
Javier <167123463+Nukkem13@users.noreply.github.com>
2025-04-12 20:26:35 -0400

    Update SignUp.tsx

--- frontend/src/pages/SignUp.tsx
+++ frontend/src/pages/SignUp.tsx
@@ -8,6 +8,11 @@ const SignUp: React.FC = () => {
   return (
     <div>
       <h1>Welcome to the SignUp Page!</h1>
+      <PageTitle />
+        <Register />
+          <button className = "SignUpButton" onClick={() => goTo('LoginPage')}>
+        <div className='ButtonText'>Return to Login</div>
+              </button>
     </div>
   );
 };
