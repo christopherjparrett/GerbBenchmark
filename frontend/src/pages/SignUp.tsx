be47b2618f63bb6112cee94783f94450f04e3ad2
Javier <167123463+Nukkem13@users.noreply.github.com>
2025-04-12 20:27:25 -0400

    Update SignUp.tsx

--- frontend/src/pages/SignUp.tsx
+++ frontend/src/pages/SignUp.tsx
@@ -5,6 +5,11 @@ import { useNavigate } from 'react-router-dom';
 import '../Styles/Logup.css';
 
 const SignUp: React.FC = () => {
+  const navigate = useNavigate();
+
+    const goTo = (inputs: string) =>{
+        navigate(`/${inputs}`);
+    };
   return (
     <div>
       <h1>Welcome to the SignUp Page!</h1>
