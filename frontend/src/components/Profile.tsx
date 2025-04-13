5bff2064bea9e440378b187814ee17cf40caa09a
Javier <167123463+Nukkem13@users.noreply.github.com>
2025-04-13 19:10:59 -0400

    Update Profile.tsx

--- frontend/src/components/Profile.tsx
+++ frontend/src/components/Profile.tsx
@@ -1,53 +1,56 @@
 import React, { useState } from 'react';
 
-function Profile(){
+function Profile() {
+  const [menuOpen, setMenuOpen] = useState(false);
 
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
 
   return (
-    <>
-          <div id="profileDiv">
-              <nav>
-                  <img src="../assets/logo.png" class="logo"/>
-                  <ul>
-                    <li><a href="#">Home</a></li>
-                    <li><a href="#">Features</a></li>
-                    <li><a href="#">About</a></li>
-                    <li><a href="#">Contact</a></li>
-                  </ul>
-                  <img src="../assets/user.png" class="user-pic" onclick="toggleMenu()"/>  
+    <div id="profileDiv">
+      <nav>
+        <img src="../assets/logo.png" className="logo" alt="logo" />
 
-                  <div class="sub-menu-wrap" id="subMenu">
-                    <div class="sub-menu">
-                      <div class="user-info">
-                        <img src="../assets/user.png"></img> 
-                        <h3>Matthew Gerber</h3>  
-                      </div>
-                        <hr></hr>
+        <ul>
+          <li><a href="#">Home</a></li>
+          <li><a href="#">Features</a></li>
+          <li><a href="#">About</a></li>
+          <li><a href="#">Contact</a></li>
+        </ul>
 
-                        <a href="#" class="sub-menu-link"/>
-                          <img src="../assets/logo.png"/>
-                          <p>Log Out</p>
-                            <span> a </span>
+        <img
+          src="../assets/user.png"
+          className="user-pic"
+          onClick={toggleMenu}
+          alt="user"
+        />
 
-                        <a href="#" class="sub-menu-link"/>
-                          <img src="../assets/logo.png"/>
-                          <p>Delete User</p>
-                            <span> a </span>
-                    </div>
-                  </div>
-                    
-              </nav>
+        <div className={`sub-menu-wrap ${menuOpen ? 'open-menu' : ''}`} id="subMenu">
+          <div className="sub-menu">
+            <div className="user-info">
+              <img src="../assets/user.png" alt="user" />
+              <h3>Matthew Gerber</h3>
+            </div>
+            <hr />
+
+            <a href="#" className="sub-menu-link">
+              <img src="../assets/logo.png" alt="logo" />
+              <p>Log Out</p>
+              <span> a </span>
+            </a>
+
+            <a href="#" className="sub-menu-link">
+              <img src="../assets/logo.png" alt="logo" />
+              <p>Delete User</p>
+              <span> a </span>
+            </a>
           </div>
-
-        <script>
-          let subMenu = document.getElementById("subMenu");
-
-          function toggleMenu(){
-            subMenu.classList.toggle("open-menu")
-          }
-        </script>
-      </>
+        </div>
+      </nav>
+    </div>
   );
-};
+}
 
 export default Profile;
+
