4abbf55ba98f42e95d1d1bc572e4d25ea617c5c0
Javier <167123463+Nukkem13@users.noreply.github.com>
2025-04-14 00:20:36 -0400

    Update Reaction.tsx

--- frontend/src/pages/Reaction.tsx
+++ frontend/src/pages/Reaction.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import Profile from '../components/Profile.tsx';
 
 const Reaction: React.FC = () => {
   return (
     <div>
+      <Profile />
       <h1>Welcome to the Reaction Page!</h1>
     </div>
   );
