e2bc93fd1b3a8ba241dc701e0eb377ad357ef99c
Javier <167123463+Nukkem13@users.noreply.github.com>
2025-04-14 00:27:26 -0400

    Update Color.tsx

--- frontend/src/pages/Color.tsx
+++ frontend/src/pages/Color.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import Profile from '../components/Profile.tsx';
 
 const Color: React.FC = () => {
   return (
     <div>
+      <Profile />
       <h1>Welcome to the Color Page!</h1>
     </div>
   );
