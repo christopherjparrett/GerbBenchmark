ee2234150380c2cabd6caa4dce65e7da00a64d94
christopherjparrett <146243093+christopherjparrett@users.noreply.github.com>
2025-04-13 20:32:08 -0400

    big changes

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -3,7 +3,8 @@ import { useNavigate } from 'react-router-dom';
 import Profile from '../components/Profile.tsx';
 import '../Styles/Home.css';
 import ColorPNG from '../assets/ColorsPNG.png'
-import GerbPNG from '../assets/Gerbster.png'
+import TypingPNG from '../assets/TypingPNG.png'
+import ReactionPNG from '../assets/ReactionPNG.png'
 const Home: React.FC = () => {
     const navigate = useNavigate();
 
@@ -14,23 +15,51 @@ const Home: React.FC = () => {
 
 
   return (
-    <div className="Overlord" style={{backgroundImage: `url(${GerbPNG})`}}>
-        <Profile />
+    <div className="Overlord">
         <div className='Banner'>
             <h1>Welcome to the Home Page!</h1>
         </div>
-      <button className = "HomeScreenButton" onClick={() => goTo('Color')}>
-        <img src={ColorPNG} alt="Colors Button Logo" className="ButtonImage"></img>
-        <div className='ButtonText'>Go to Color Test</div>
-      </button>
-      <button className = "HomeScreenButton" onClick={() => goTo('Typing')}>
-        <img src={ColorPNG} alt="Typing Button Logo" className="ButtonImage"></img>
-        <div className='ButtonText'>Go to Typing Test</div>
-      </button>
-      <button className = "HomeScreenButton" onClick={() => goTo('Reaction')}>
-        <img src={ColorPNG} alt="Reaction Button Logo" className="ButtonImage"></img>
-        <div className='ButtonText'>Go to Reaction Test</div>
-      </button>
+        <div className='AllButtons'>
+                <button className = "HomeScreenButton" onClick={() => goTo('Color')}>
+                    <div className="ButtonFront">
+                        <img src={ColorPNG} alt="Colors Button Logo" className="ButtonImage"></img>
+                        <div className='ButtonText'>Color Comprehension Test</div>
+                    </div>
+                    <div className="ButtonBack">
+                        <div className='ButtonText'>
+                            Test your color recognition skills!
+                            Multiple squares will appear on the screen and one will be slightly different.
+                            Try to identify the different square and get as many points as you can!
+                        </div>
+                    </div>
+                </button>
+                <button className = "HomeScreenButton" onClick={() => goTo('Typing')}>
+                <div className="ButtonFront">
+                        <img src={TypingPNG} alt="Colors Button Logo" className="ButtonImage"></img>
+                        <div className='ButtonText'>Typing Speed Test</div>
+                    </div>
+                    <div className="ButtonBack">
+                        <div className='ButtonText'>
+                            Test your Typing skills!<br></br>
+                            A sentence will appear on the screen and you will have to type it as fast as you can!<br></br>
+                            Wrong inputs will be penalized, so be careful!
+                        </div>
+                    </div>
+                </button>
+                <button className = "HomeScreenButton" onClick={() => goTo('Reaction')}>
+                <div className="ButtonFront">
+                        <img src={ReactionPNG} alt="Colors Button Logo" className="ButtonImage"></img>
+                        <div className='ButtonText'>Reaction Time Test</div>
+                    </div>
+                    <div className="ButtonBack">
+                        <div className='ButtonText'>
+                            How fast can you react<br></br>
+                            The screen will change after some time and you are supposed to press the button as fast as you can!<br></br>
+                            
+                        </div>
+                    </div>
+                </button>
+        </div>
     </div>
   );
 };
