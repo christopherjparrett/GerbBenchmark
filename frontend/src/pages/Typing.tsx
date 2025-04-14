089ee3392d9e644f80209b55929c26935f8e292d
Javier <167123463+Nukkem13@users.noreply.github.com>
2025-04-14 00:26:42 -0400

    Update Typing.tsx

--- frontend/src/pages/Typing.tsx
+++ frontend/src/pages/Typing.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import Profile from '../components/Profile.tsx';
 import TypingGame from '../components/TypingGame.tsx';
 import '../Styles/Typing.css';
 const Typing: React.FC = () => {
   return (
     <div>
+      <Profile />
       <TypingGame />
     </div>
   );
