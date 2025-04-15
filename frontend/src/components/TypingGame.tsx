ac117e56db3c5b5857eadc204602170594111b2e
kylekratt <kylekratt@gmail.com>
2025-04-14 23:07:36 -0400

    Trying again

--- frontend/src/components/TypingGame.tsx
+++ frontend/src/components/TypingGame.tsx
@@ -51,7 +51,7 @@ function TypingGame() {
             length.current = msg.current.length;
         }
         document.getElementById("output")?.style.setProperty("border", "1px solid white");
-        document.removeEventListener("keydown", (e) => {checkChar(e)})
+        document.removeEventListener("keydown", checkChar)
         document.addEventListener("keydown", (e) => { checkChar(e) })
     }
 
