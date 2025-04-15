d35638777258cd083c354cdc6f6b91a27cf0426b
RangerHalt2 <141537190+RangerHalt2@users.noreply.github.com>
2025-04-14 21:42:17 -0400

    Missed Semicolon

--- server.js
+++ server.js
@@ -141,7 +141,7 @@ app.post('/api/pullLeaderBoard', async (req, res, next) => {
     const db = client.db();
     var doBool = true;
     var top10;
-    const filtered
+    const filtered;
     const { gameId: game } = req.body;
 
     if (!game) {
